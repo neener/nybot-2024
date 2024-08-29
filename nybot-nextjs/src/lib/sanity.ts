c1060fff47f04d820be9648587fe84e6522ff563
neener <janinemrosen@gmail.com>
2024-08-29 12:53:39 -0400

    heh

--- nybot-nextjs/src/lib/sanity.ts
+++ nybot-nextjs/src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'hd3sdon3',
-  dataset: 'production',
-  apiVersion: '2023-07-24', // use current UTC date
-  useCdn: false,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'fallbackProjectID', // Ensure fallback if env is missing
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production', // Default to production dataset
+  apiVersion: '2023-07-24',
+  useCdn: process.env.NODE_ENV === 'production', // Enable CDN for production
 });
