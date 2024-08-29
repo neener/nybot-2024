a6ffb636493be63715d9b4ece72e271ef18f93bf
neener <janinemrosen@gmail.com>
2024-08-29 13:17:25 -0400

    api version update

--- nybot-nextjs/src/lib/sanity.ts
+++ nybot-nextjs/src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'fallbackProjectID', // Ensure fallback if env is missing
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production', // Default to production dataset
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION, // The API version from your .env file
+export const sanityClient = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,  // Your Sanity project ID
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,      // Your Sanity dataset (e.g., 'production')
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-07-24', // Use the API version from your .env file or a default value
   useCdn: true,  // Set to true for faster response times (good for public data)
 });
